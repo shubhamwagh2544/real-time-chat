d2c8e9e9ea83e085b24c5a5846b82bae947ecc52
Shubham Wagh <shubhamwagh2544@gmail.com>
2024-07-27 14:15:42 +0530

    ws updated to websocket

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,57 @@
-import { WebSocketServer } from "ws";
+import { connection, server as WebSocketServer } from "websocket";
 import http from "http";
+import { UserManager } from "./UserManager";
+
+import { InMemoryStore } from "./store/InMemoryStore";
+import { IncomingMessage, SupportedMessage } from "./message";
 
 const server = http.createServer(function (request: any, response: any) {
+  console.log(new Date() + " Received request for " + request.url);
   response.writeHead(404);
   response.end();
 });
 
-const wss = new WebSocketServer({
-  port: 8080,
-  server: server,
+const userManager = new UserManager();
+const store = new InMemoryStore();
+
+server.listen(8080, function () {
+  console.log(new Date() + " Server is listening on port 8080");
 });
 
-wss.on('connection', function connection(ws) {
-  ws.on('error', console.error);
-  ws.on('message', function (message) {
-  })
+const wsServer = new WebSocketServer({
+  httpServer: server,
+  autoAcceptConnections: false,
 });
+
+function originIsAllowed(origin: string) {
+  return true;
+}
+
+wsServer.on("request", function (request) {
+  console.log("inside connect");
+
+  if (!originIsAllowed(request.origin)) {
+    // Make sure we only accept requests from an allowed origin
+    request.reject();
+    console.log(new Date() + " Connection from origin " + request.origin + " rejected.");
+    return;
+  }
+
+  const connection = request.accept("echo-protocol", request.origin);
+  console.log(new Date() + " Connection accepted.");
+  connection.on("message", function (message) {
+    // Todo add rate limitting logic here
+    if (message.type === "utf8") {
+      try {
+        messageHandler(JSON.parse(message.utf8Data), connection);
+      } catch (e) {}
+    }
+  });
+});
+
+function messageHandler(message: IncomingMessage, connection: connection) {
+  if (message.type == SupportedMessage.JoinRoom) {
+    const payload= message.payload;
+    userManager.addUser(payload.name, payload.userId, payload.roomId, connection);
+  }
+}
